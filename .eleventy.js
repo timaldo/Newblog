5a5f5eabb7bae0e402958565647750f2f8c4b8de
timaldo <info@timdorsett.com>
2021-06-27 16:01:55 +0100

    fixed admin panel

--- .eleventy.js
+++ .eleventy.js
@@ -3,6 +3,8 @@ const { DateTime } = require('luxon');
 module.exports = function (eleventyConfig) {
   eleventyConfig.addPassthroughCopy('./src/style.css');
   eleventyConfig.addPassthroughCopy('./src/assets');
+  eleventyConfig.addPassthroughCopy('./src/admin');
+
   eleventyConfig.addShortcode('year', () => `${new Date().getFullYear()}`);
 
   eleventyConfig.addFilter('postDate', dateObj => {
